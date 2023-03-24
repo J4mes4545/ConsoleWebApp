b2cab350e7ccd5311f1f61582540dcc3ad036717
James45 <120043011+J4mes4545@users.noreply.github.com>
2023-03-24 11:09:52 -0500

    Main

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "James "
+var last_name = "Peters"
+
+var a = 10;
+var b = 5;
+var add = a+b;
+
+console.log(a);
+console.log(b);
+console.log(first_name);
+console.log(last_name);
+console.log(add);
